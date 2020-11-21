5d3ea98166291488d3f8fdb44be9fa224a8232d8
PavelKimm <fastredialer@gmail.com>
2020-11-21 22:22:45 +0600

    fix

--- films_repository.py
+++ films_repository.py
@@ -1,25 +1,23 @@
 from typing import List, Tuple
-
-from Film import Film
 from db_interface import query_db, write_db, get_db
 
 
-def get_films_to_compare() -> Tuple[Film, Film]:
-    films: List[Film] = query_db('SELECT * FROM films WHERE id IN (SELECT id FROM films ORDER BY RANDOM() LIMIT 2)')
+def get_films_to_compare():
+    films = query_db('SELECT * FROM films WHERE id IN (SELECT id FROM films ORDER BY RANDOM() LIMIT 2)')
     return films[0], films[1]
 
 
 def save_comparison(win_id: int, lose_id: int):
-    win_film: Film = get_film(win_id)
+    win_film = get_film(win_id)
     update_film_rating(win_id, win_film['win'] + 1, win_film['lose'])
 
-    lose_film: Film = get_film(lose_id)
+    lose_film = get_film(lose_id)
     update_film_rating(lose_id, lose_film['win'], lose_film['lose'] + 1)
 
     get_db().commit()
 
 
-def get_film(id_: int) -> Film:
+def get_film(id_: int):
     return query_db('SELECT * FROM films WHERE id = ?', [id_], True)
 
 
@@ -27,5 +25,5 @@ def update_film_rating(id_: int, win: int, lose: int):
     write_db('UPDATE films SET win = ?, lose = ?, rating = ? WHERE id = ?', [win, lose, win / (win + lose), id_])
 
 
-def get_rating(n: int = 160) -> List[Film]:
+def get_rating(n: int = 160):
     return query_db('SELECT * from films ORDER BY rating DESC LIMIT ?', [n])
